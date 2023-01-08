8ec301c924848cdd2d54afb1078fe644304fb3e9
NoraA8 <zaske8@gmail.com>
2023-01-08 16:30:39 -0300

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
