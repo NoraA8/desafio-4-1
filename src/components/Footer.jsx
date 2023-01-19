2966f913ffd3d6a5d23d1d8b0af25a68493cab1a
NoraA8 <zaske8@gmail.com>
2023-01-19 17:18:44 -0300

    changes css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = (props) => {
-  return <p className="text-center bg-success py-2"><span class="badge bg-success">{props.text}</span></p>;
+  return <p className="text-center bg-success py-2 m-0"><span class="badge bg-success">{props.text}</span></p>;
 };
 
 export default Footer;
